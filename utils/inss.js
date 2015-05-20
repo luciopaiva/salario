c4e897f333be5db35a34079fb629b162019f5b2e
Lucio Paiva <luciopaiva@gmail.com>
2015-05-19 23:48:32 -0300

    Update INSS table to 2015

--- utils/inss.js
+++ utils/inss.js
@@ -6,27 +6,29 @@
 
     function INSS() {
         /**
-         * Tabela do INSS para 2014.
+         * Tabela do INSS para 2015.
          * A posição 0 é um limiar em centavos de reais e a posição 1 é o percentual a ser aplicado.
+         * 
+         * http://agencia.previdencia.gov.br/e-aps/servico/147
          **/
-        var inssTable2014 = [
-            [131707, .08],
-            [219512, .09],
-            [439024, .11]
+        var inssTable = [
+            [139912, .08],
+            [233188, .09],
+            [466375, .11]
         ];
 
         // Teto a partir do qual a contribuição chega num limite.
-        var ceiling = inssTable2014[inssTable2014.length - 1][0] * inssTable2014[inssTable2014.length - 1][1];
-        var individual = Math.round(.20 * inssTable2014[inssTable2014.length - 1][0]);
+        var ceiling = inssTable[inssTable.length - 1][0] * inssTable[inssTable.length - 1][1];
+        var individual = Math.round(.20 * inssTable[inssTable.length - 1][0]);
 
         /**
          * Calcula a contribuição devida ao INSS (em centavos de reais) para um determinado salário passado como parâmetro.
          **/
-        function inss2014(value) {
+        function inss(value) {
             var
                 result = ceiling;
 
-            inssTable2014.some(function (thres) {
+            inssTable.some(function (thres) {
                 if (value < thres[0]) {
                     result = thres[1] * value;
                     return true;
@@ -47,12 +49,12 @@
         /**
          * Retorna a contribuição devida ao INSS (em centavos de reais) para um contribuinte individual.
          **/
-        function inss2014individual() {
+        function inssIndividual() {
             return individual;
         }
 
         return {
-            calculate: inss2014
+            calculate: inss
         };
     }
 
